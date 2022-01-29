d9eab3e2669a34e7b9fb62d5f5cfca99a3d9e564
Haron Tavares <haroncesar@gmail.com>
2022-01-29 20:32:36 -0300

    Aula 08

--- src/views/Servico/Cadastrar/index.js
+++ src/views/Servico/Cadastrar/index.js
@@ -1,10 +1,51 @@
+import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Container, Form, FormGroup, Input, Label } from "reactstrap"
+import { Alert, Button, Container, Form, FormGroup, Input, Label } from "reactstrap"
+import { api } from "../../../config";
 
 export const Cadastrar = () => {
 
-    const cadServico = async () => {
-        console.log('Cadastrar');
+    const [servico, setServico] = useState({
+        nome: '',
+        descricao: ''
+    });
+
+    const [status, setStatus] = useState({
+        type: '',
+        message: ''
+    });
+
+    const valorInput = e => setServico({
+        ...servico, [e.target.name]: e.target.value
+    });
+
+    const cadServico = async e => {
+        e.preventDefault();
+        console.log(servico);
+
+        const headers = {
+            'Content-Type': 'application/json'
+        };
+
+        await axios.post(api + '/servicos', servico, { headers })
+            .then((response) => {
+                // console.log(response.data.message);
+                if (response.data.error) {
+                    setStatus({
+                        type: 'error',
+                        message: response.data.message
+                    });
+                } else {
+                    setStatus({
+                        type: 'success',
+                        message: response.data.message
+                    });
+                }
+            })
+            .catch(() => {
+                console.log('Erro: Sem conexão com a API.');
+            });
     };
 
     return (
@@ -20,15 +61,22 @@ export const Cadastrar = () => {
             </div>
 
             <hr className='m-1' />
+
+            {status.type === 'error' ? <Alert color='danger'>{status.message}</Alert> : ''}
+
+            {status.type === 'success' ? <Alert color='success'>{status.message}</Alert> : ''}
+
             <Form className='p-2' onSubmit={cadServico}>
                 <FormGroup className='p-2'>
                     <Label>Nome</Label>
-                    <Input type="text" name="nome" placeholder="Nome do serviço" />
+                    <Input type="text" name="nome" placeholder="Nome do serviço"
+                        onChange={valorInput} />
                 </FormGroup>
 
                 <FormGroup className='p-2'>
                     <Label>Descrição</Label>
-                    <Input type="text" name="descricao" placeholder="Descrição do serviço" />
+                    <Input type="text" name="descricao" placeholder="Descrição do serviço"
+                        onChange={valorInput} />
                 </FormGroup>
 
                 <Button type='submit' outline color='success'>Cadastrar</Button>
